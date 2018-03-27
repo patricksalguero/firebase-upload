b7efb60833e43a10723d6d90b74dd14ac1856950
Patrick Salguero <patrick.salguero.avalos@gmail.com>
2018-03-27 00:05:12 -0500

    Se compila la version 0.1.0

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyCY4HbFupstQcYK9l1S2MUn_INdU_QFhJA",
+    authDomain: "fire-upload-392e8.firebaseapp.com",
+    databaseURL: "https://fire-upload-392e8.firebaseio.com",
+    projectId: "fire-upload-392e8",
+    storageBucket: "fire-upload-392e8.appspot.com",
+    messagingSenderId: "672643769662"
+  }
 };
